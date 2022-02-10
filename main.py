39acfcd028e7f8874863677ef6d518b743dcec1c
Illia <illiakucherenko95@gmail.com>
2022-02-10 12:22:54 +0200

    1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 source1 = input('input string 1> ')
 source2 = input('input string 2> ')
 
-print(source1 * 5)  # duplication
-print(source1 + source2)  # concatenation
-print(len(source1) + len(source2))  # length concatenation
+print(source1 * 5)  # str duplication
+print(source1 + source2)  # str concatenation
+print(len(source1) + len(source2))  # str length concatenation
 if source1.find(source2):  # always finds substring if str2 has any character
     print('substring found')
 else:
